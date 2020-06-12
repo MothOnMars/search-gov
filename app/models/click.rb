9f8d15565bd3669e3313cfe2aec9050380a4239e
MothOnMars <437455+MothOnMars@users.noreply.github.com>
2020-06-12 14:09:12 -0700

    search modules const

--- app/models/click.rb
+++ app/models/click.rb
@@ -12,14 +12,14 @@ class Click
   validates :position, numericality: { only_integer: true, greater_than_or_equal_to: 0, allow_blank: true }
   validates :module_code,
     inclusion: {
-      in: MODULES,
+      in: SEARCH_MODULES,
       allow_blank: true, message: '%{value} is not a valid module'
   }
   #validates :module_code, inclusion: { in: ["BBG", "BOOS", "BWEB", "CREL", "QRTD", "VIDEO"], allow_blank: true, message: '%{value} is not a valid module' }
   validate :client_ip_validation
 
   def initialize(params)
-    puts "MODULES: #{SearchModule.pluck(:tag)}"
+    puts "MODULES: #{MODULES}"
     @url = unescape_url(params[:url])
     @query = params[:query]
     @client_ip = params[:client_ip]
