ba279f7e1966936372709f16a28048d435e08b21
MothOnMars <437455+MothOnMars@users.noreply.github.com>
2020-06-12 13:02:47 -0700

    one spec

--- app/models/click.rb
+++ app/models/click.rb
@@ -7,7 +7,11 @@ class Click
 
   validates :url, :query, :position, :module_code, :client_ip, :user_agent, presence: true
   validates :position, numericality: { only_integer: true, greater_than_or_equal_to: 0, allow_blank: true }
-  validates :module_code, inclusion: { in: SearchModule.pluck(:tag), allow_blank: true, message: '%{value} is not a valid module' }
+  validates :module_code,
+    inclusion: {
+      in: SearchModule.pluck(:tag),
+      allow_blank: true, message: '%{value} is not a valid module'
+  }
   #validates :module_code, inclusion: { in: ["BBG", "BOOS", "BWEB", "CREL", "QRTD", "VIDEO"], allow_blank: true, message: '%{value} is not a valid module' }
   validate :client_ip_validation
 
