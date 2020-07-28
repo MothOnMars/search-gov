918b08cc04f9dc6bef15e68a1b8aa0e0203e189f
MothOnMars <437455+MothOnMars@users.noreply.github.com>
2020-07-28 12:39:36 -0700

    job

---
+++ app/jobs/searchgov_domain_reindexer_job.rb
@@ -0,0 +1,8 @@
+class SearchgovDomainReindexerJob < ApplicationJob
+  queue_as :searchgov
+
+  def perform(searchgov_domain:)
+    searchgov_domain.searchgov_urls.ok.in_batches.update_all(enqueued_for_reindex: true)
+    searchgov_domain.index_sitemaps
+  end
+end
